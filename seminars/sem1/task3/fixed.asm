e8bde6bdabdc10dbeadb68ba3cc173e0edc8db76
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-24 19:39:44 +0300

    add fixed.asm

---
+++ seminars/sem1/task3/fixed.asm
@@ -0,0 +1,23 @@
+; fixed.asm
+  section .data
+  message: db 'Hello, world!', 10
+
+  section .text
+  global _start
+
+  _start:
+        mov rax, 1      ; 'write' syscall number
+        mov rdi, 1      ; stdout descriptor
+        mov rsi, message        ; string address
+        mov rdx, 14     ; string length in bytes
+        syscall
+
+        mov rax, 1
+        mov rdi, 2      ; stderr descriptor
+        mov rsi, message
+        mov rdx, 14
+        syscall
+
+        mov rax, 60     ; 'exit' syscall number
+        xor rdi, rdi
+        syscall
