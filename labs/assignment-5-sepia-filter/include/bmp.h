b758aa4cdbd924900248c733c5b26fd991cd4a11
Mikhailov Dmitry <misticslippers@gmail.com>
2026-04-23 08:48:55 +0300

    add bmp.h

---
+++ labs/assignment-5-sepia-filter/include/bmp.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdio.h>
+
+#include "image.h"
+
+enum read_status {
+    READ_OK = 0,
+    READ_INVALID_SIGNATURE,
+    READ_INVALID_BITS,
+    READ_INVALID_HEADER,
+    READ_INVALID_BODY,
+    READ_MEMORY_ERROR,
+    READ_IO_ERROR
+};
+
+enum write_status {
+    WRITE_OK = 0,
+    WRITE_ERROR
+};
+
+enum read_status from_bmp(FILE *in, struct image *img);
+enum write_status to_bmp(FILE *out, const struct image *img);
