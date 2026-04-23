6230b8e6354dbac2c118695fd0fc3a3bf3df0c5a
Mikhailov Dmitry <misticslippers@gmail.com>
2026-04-23 08:51:52 +0300

    add dimensions.c

---
+++ labs/assignment-5-sepia-filter/src/dimensions.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+
+#include "dimensions.h"
+
+struct dimensions dimensions_reverse(const struct dimensions *dim) {
+    struct dimensions result = {0};
+
+    if (dim == NULL) {
+        return result;
+    }
+
+    result.x = dim->y;
+    result.y = dim->x;
+    return result;
+}
